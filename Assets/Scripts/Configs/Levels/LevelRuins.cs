4cb09f2d37fbf600311e059a5c5bfdad254505a2
Almaz Kinziabulatov <auqolaq@gmail.com>
2021-01-15 21:54:06 +0300

    DB-53 #review #comment Ruins level config script and file have been added.

---
+++ Assets/Scripts/Configs/Levels/LevelRuins.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Configs.Levels
+{
+    [CreateAssetMenu(fileName = "LevelRuinsConfig", menuName = "Configs/Level Configs/Level Ruins Config", order = 2)]
+    public class LevelRuins : ScriptableObject
+    {
+        [Header("General Configs")]
+        public float Width;
+        public float Height;
+
+        public int CellsNumberInRows;
+        public int CellsNumberInColumns;
+
+        [Header("Cells' Assets Prefabs")]
+        public GameObject WallCellPrefab;
+        public GameObject FloorCellPrefab;
+        public GameObject ConcreteWallCellPrefab;
+
+        public GameObject LevelBackgroundPrefab;
+    }
+}
