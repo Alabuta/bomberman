2d2a283c7b35e615b3c93f4f5dc65eee05c198e9
Almaz Kinziabulatov <auqolaq@gmail.com>
2021-01-23 14:58:48 +0300

    DB-66 #review #comment A few tiles' config files have been added.

---
+++ Assets/Scripts/Configs/Level/Tile/BreakableTileConfig.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Configs.Level.Tile
+{
+    [CreateAssetMenu(fileName = "BreakableTileConfig", menuName = "Configs/Level/Breakable Tile Config", order = 5)]
+    public class BreakableTileConfig : TileConfig
+    {
+
+    }
+}
