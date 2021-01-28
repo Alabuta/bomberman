f3c5c556856798b4d5b327b50c823bfe74fd8f37
Almaz Kinziabulatov <auqolaq@gmail.com>
2021-01-28 23:20:52 +0300

    Work in progress.

--- Assets/Scripts/App/Level/LevelGrid.cs
+++ Assets/Scripts/App/Level/LevelGrid.cs
@@ -16,13 +16,13 @@ namespace App.Level
         public int this[int2 position] => _grid[position.x + position.y * ColumnsNumber];
         public int this[int index] => _grid[index];
 
-        private readonly int[] _grid;
+        private /*readonly*/ int[] _grid;
 
         public LevelGrid(LevelConfig levelConfig)
         {
             _levelConfig = levelConfig;
 
-            _grid = Enumerable.Repeat(0, RowsNumber * ColumnsNumber).ToArray();
+            // _grid = Enumerable.Repeat(0, RowsNumber * ColumnsNumber).ToArray();
 
             Generate();
         }
@@ -42,11 +42,11 @@ namespace App.Level
 
             var rg = new Random();
 
-            /*int[] reps = {emptyCellsNumber, hardBlocksNumber};
+            int[] reps = {emptyCellsNumber, softBlocksNumber};
 
             _grid = Enumerable.Range(0, 2)
                 .SelectMany(i => Enumerable.Repeat(i, reps[i]))
-                .OrderBy(t => rg.Next()).ToArray();*/
+                .OrderBy(t => rg.Next()).ToArray();
 
             for (var rowIndex = 0; rowIndex < RowsNumber; ++rowIndex)
             {
@@ -66,3 +66,76 @@ namespace App.Level
         }
     }
 }
+
+/*
+ * var random = new Random();
+var (ColumnsNumber, RowsNumber) = (7, 5);
+
+var playersNumber = 1;
+
+var totalCellsNumber = ColumnsNumber * RowsNumber;
+var hardBlocksNumber = (ColumnsNumber - 1) * (RowsNumber - 1) / 4;
+// var emptyCellsNumber = totalCellsNumber - hardBlocksNumber;
+
+var softBlocksNumber = (totalCellsNumber - hardBlocksNumber) * 0 / 100;
+var emptyCellsNumber = totalCellsNumber - softBlocksNumber - hardBlocksNumber;
+
+int[] ocpd = {0, 1, 7, 8, 10, 12};
+int[] reps = {emptyCellsNumber, hardBlocksNumber, softBlocksNumber};
+Console.Write($"{emptyCellsNumber}, {hardBlocksNumber}, {softBlocksNumber}");
+var k = -1;
+// var edge = -1;
+var sidx = 0;
+int[] tttt = {0, 0, 0};
+var numbers = Enumerable.Range(0, 3)
+       .SelectMany(i => Enumerable.Repeat(i, reps[i]))
+       .OrderBy(t => random.Next())
+       .Select((e, i) => {
+            var (columnIndex, rowIndex) = (i % ColumnsNumber, i / ColumnsNumber);
+            if (columnIndex % 2 == 1 && rowIndex % 2 == 1) {
+                if (e != 1) {
+                    ++tttt[e];
+                }
+
+                return 1;
+            }
+
+            else {
+                if (e == 1) {
+                    var d = tttt.Select((value, index) => new {value, index})
+                                .OrderByDescending(vi => vi.value)
+                                .First();
+
+                    --tttt[d.index];
+                    return d.index;
+                }
+            }
+
+            return e;
+       })
+       .ToArray();
+
+// var rep2 = {emptyCellsNumber, softBlocksNumber};
+// var number = Enumerable.Range(0, totalCellsNumber)
+//     .
+
+var j = 0;
+foreach (var num in numbers)
+{
+    Console.Write($"{(j % 7 == 0 ? '\n' : ' ')}{num}");
+    ++j;
+}
+
+Console.Write("\n");
+
+Console.WriteLine("\n================================");
+
+j = 0;
+foreach (var num in numbers)
+{
+    Console.Write($"{(j % 7 == 0 ? '\n' : ' ')}{num}");
+    ++j;
+}
+
+
+ */
