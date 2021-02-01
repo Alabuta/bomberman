c8e678f422f9512bc600c4409f1882d8d35c467d
Almaz Kinziabulatov <auqolaq@gmail.com>
2021-02-01 21:30:26 +0300

    Minor changes.

--- Assets/Scripts/Configs/Level/Tile/PortalBlock.cs
+++ Assets/Scripts/Configs/Level/Tile/PortalBlock.cs
@@ -6,6 +6,5 @@ namespace Configs.Level.Tile
     [CreateAssetMenu(fileName = "PortalBlockConfig", menuName = "Configs/Level/Portal Block Config")]
     public sealed class PortalBlock : BlockConfig
     {
-        public EnemyConfig[] Enemies;
     }
 }
--- Assets/Scripts/Configs/Level/Tile/SoftBlock.cs
+++ Assets/Scripts/Configs/Level/Tile/SoftBlock.cs
@@ -5,7 +5,5 @@ namespace Configs.Level.Tile
     [CreateAssetMenu(fileName = "SoftBlockConfig", menuName = "Configs/Level/Soft Block Config")]
     public sealed class SoftBlock : BlockConfig
     {
-        public float MovementSpeedMultiplier;
-        public float Strengh;
-    }
+        }
 }
