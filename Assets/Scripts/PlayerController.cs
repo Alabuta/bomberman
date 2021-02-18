cc0fa8d66c755cf9d2a303b9dc7081da7b52259e
Almaz Kinziabulatov <auqolaq@gmail.com>
2021-02-18 20:26:00 +0300

    DB-74 #comment Animations and player controller.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,30 @@
+using System;
 using Unity.Mathematics;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
+    private static readonly int VerticalSpeed = Animator.StringToHash("VerticalSpeed");
+    private static readonly int HorizontalSpeed = Animator.StringToHash("HorizontalSpeed");
+
     public float Speed;
 
     private float2 _movementVector = float2.zero;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = gameObject.GetComponent<Animator>();
+    }
 
     private void Update()
     {
         _movementVector.x = Input.GetAxis("Horizontal");
         _movementVector.y = Input.GetAxis("Vertical");
+
+        _animator.SetFloat(HorizontalSpeed, _movementVector.x);
+        _animator.SetFloat(VerticalSpeed, _movementVector.y);
 
         _movementVector = math.round(_movementVector) * Speed;
     }
