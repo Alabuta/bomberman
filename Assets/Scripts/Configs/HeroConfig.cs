4a4ceee63b572ddeae6b98ea755d36d63849429a
Almaz K <auqolaq@gmail.com>
2021-02-19 09:10:57 +0300

    feat/DB-74 Bomberman animations (#21)
    * DB-71 #comment Colliders.
    
    * DB-74 #comment Animations and player controller.
    
    * DB-74 #review #comment Several bomberman animations.

--- Assets/Scripts/Configs/HeroConfig.cs
+++ Assets/Scripts/Configs/HeroConfig.cs
@@ -2,14 +2,11 @@ using UnityEngine;
 
 namespace Configs
 {
-    [CreateAssetMenu(fileName = "HeroConfig", menuName = "Configs/Hero Config", order = 3)]
+    [CreateAssetMenu(fileName = "HeroConfig", menuName = "Configs/Hero Config")]
     public sealed class HeroConfig : ScriptableObject
     {
         [Header("Movement Configs"), Range(0f, 10f)]
-        public float SpeedValue;
-
-        [Range(1f, 3f)]
-        public float SpeedBoostMultiplier;
+        public float Speed;
 
         [Header("Hero Health Configs"), Range(0, 5)]
         public int MaxHealtsPoints;
