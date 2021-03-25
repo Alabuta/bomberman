ea93e6afc62e54dda4d342399b19c6af0c44b67c
Almaz Kinziabulatov <auqolaq@gmail.com>
2021-03-25 21:41:30 +0300

    DB-81 #review #comment Bomb config and prefab.

---
+++ Assets/Scripts/Configs/Entity/BombConfig.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Configs.Entity
+{
+    [CreateAssetMenu(fileName = "BombConfig", menuName = "Configs/Entity/Bomb Config")]
+    public sealed class BombConfig : ScriptableObject
+    {
+        [Header("General Parameters")]
+        public string Name;
+
+        public GameObject Prefab;
+    }
+}
