6dc0546b361d92709476cc586550e70c97bdad16
Almaz Kinziabulatov <auqolaq@gmail.com>
2021-03-25 20:29:58 +0300

    DB-79 #review #comment Entity and player controllers first approach.

---
+++ Assets/Scripts/Entity/IPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Entity
+{
+    public interface IPlayer : IEntity
+    {
+        event EventHandler<BombPlantEventData> BombPlantedEvent;
+
+        int BlastRadius { get; set; }
+
+        int BombCapacity { get; set; }
+    }
+}
