b8d3f49accea605fea16bfbfdc66c81b8273aba9
Almaz K <auqolaq@gmail.com>
2022-02-26 00:01:55 +0300

    Minor changes.

---
+++ Assets/Scripts/App/BootstrapInstaller.cs
@@ -0,0 +1,29 @@
+﻿/*using Infrastructure.Services.Input;
+using Zenject;
+
+namespace App
+{
+    public class BootstrapInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            BindInputService();
+        }
+
+        private void BindInputService()
+        {
+            Container
+                .Bind<IInputService>()
+                .To<InputService>()
+                // .FromInstance(Infrastructure.Game.InputService)
+                .AsSingle();
+            /*var inputService = Container
+                .Bind<IInputService>()
+                .To<InputService>()
+                .FromComponentInNewPrefab(inputServicePrefab)
+                .AsSingle();#1#
+        }
+    }
+}*/
+
+
---
+++ Assets/Scripts/App/LocationInstaller.cs
@@ -0,0 +1,20 @@
+/*
+using Zenject;
+
+namespace App
+{
+    public class LocationInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            /*var heroController = Container.InstantiatePrefabForComponent<HeroController>(heroPrefab);
+            Container
+                .Bind<HeroController>()
+                .FromInstance(heroController)
+                .AsCached();#1#
+        }
+    }
+}
+*/
+
+
