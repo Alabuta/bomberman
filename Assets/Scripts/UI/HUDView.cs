d1735cfb733ec9640c8b8a73801c5d833957a99e
Almaz K <auqolaq@gmail.com>
2022-03-10 21:31:17 +0300

    Game HUD in progress.

---
+++ Assets/Scripts/UI/HUDView.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class HUDView : MonoBehaviour
+    {
+        [SerializeField]
+        private Image HeroIcon;
+
+        [SerializeField]
+        private TextMeshProUGUI HeroHpText;
+
+        public void SetHeroIcon(Sprite sprite)
+        {
+            if (HeroIcon != null)
+                HeroIcon.sprite = sprite;
+        }
+
+        public void SetHeroHp(int hp)
+        {
+            if (HeroHpText != null)
+                HeroHpText.SetText(hp.ToString());
+        }
+    }
+}
