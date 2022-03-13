2789f60cc83eed7d5bcc44878aa9a03621c4d422
Almaz K <auqolaq@gmail.com>
2022-03-13 21:42:22 +0300

    Player progress reset menu tool.

---
+++ Assets/Scripts/Editor/Tools.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    public static class Tools
+    {
+        [MenuItem("Tools/Clear Player Prefs")]
+        public static void ClearPlayerPrefs()
+        {
+            PlayerPrefs.DeleteAll();
+            PlayerPrefs.Save();
+        }
+    }
+}
