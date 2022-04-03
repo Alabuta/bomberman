bcd04b4ea4a4761bf25fa0a0396ccfc1c440ce98
Almaz K <auqolaq@gmail.com>
2022-04-03 10:33:56 +0300

    Loading screen in progress.

---
+++ Assets/Scripts/UI/LoadingScreenController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class LoadingScreenController : MonoBehaviour
+    {
+        [SerializeField]
+        private CanvasGroup CanvasGroup;
+
+        [SerializeField]
+        private TextMeshProUGUI LevelAndStageText;
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(this);
+        }
+
+        public void Show(int level, int stage)
+        {
+            if (LevelAndStageText != null)
+                LevelAndStageText.text = $"{level}<color=#F6E500><voffset=0.2em>‒</voffset></color>{stage}";
+
+            if (CanvasGroup != null)
+                CanvasGroup.alpha = 1;
+
+            gameObject.SetActive(true);
+        }
+
+        public void Hide()
+        {
+            StartCoroutine(FadeIn());
+        }
+
+        private IEnumerator FadeIn()
+        {
+            if (CanvasGroup == null)
+            {
+                gameObject.SetActive(false);
+                yield break;
+            }
+
+            while (CanvasGroup.alpha < 0)
+            {
+                CanvasGroup.alpha -= .04f;
+                yield return new WaitForSeconds(0.04f);
+            }
+
+            gameObject.SetActive(false);
+        }
+    }
+}
