9cc189a247e807ab81fcd6fc32056642b52ed65c
Almaz K <auqolaq@gmail.com>
2022-04-20 21:33:16 +0300

    Layer custom attribute and attribute drawer.

---
+++ Assets/Scripts/Core/Attributes/LayerAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+namespace Core.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class LayerAttribute : PropertyAttribute
+    {
+    }
+}
---
+++ Assets/Scripts/Editor/Attributes/LayerAttributeDrawer.cs
@@ -0,0 +1,21 @@
+﻿using Core.Attributes;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor.Attributes
+{
+    [CustomPropertyDrawer(typeof(LayerAttribute))]
+    public class LayerAttributeDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (property.propertyType != SerializedPropertyType.Integer)
+            {
+                EditorGUI.LabelField(position, "The property has to be a layer for LayerAttribute to work!");
+                return;
+            }
+
+            property.intValue = EditorGUI.LayerField(position, label, property.intValue);
+        }
+    }
+}
