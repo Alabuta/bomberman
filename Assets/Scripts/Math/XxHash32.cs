78972ee5eb1629b8b547dd0c2f7cc1a2326a7950
Almaz K <auqolaq@gmail.com>
2022-06-26 14:22:31 +0300

    xxHash32 hashing algorithm.

--- Assets/Scripts/Math/XxHash32.cs
+++ Assets/Scripts/Math/XxHash32.cs
@@ -6,79 +6,80 @@ namespace Math
     // https://github.com/uranium62/xxHash
     public static class XxHash32
     {
-        private const uint XxhPrime321 = 2654435761U;
-        private const uint XxhPrime322 = 2246822519U;
-        private const uint XxhPrime323 = 3266489917U;
-        private const uint XxhPrime324 = 668265263U;
-        private const uint XxhPrime325 = 374761393U;
+        // ReSharper disable InconsistentNaming
+        private const uint XxhPrime32_1 = 2654435761U;
+        private const uint XxhPrime32_2 = 2246822519U;
+        private const uint XxhPrime32_3 = 3266489917U;
+        private const uint XxhPrime32_4 = 668265263U;
+        private const uint XxhPrime32_5 = 374761393U;
+        // ReSharper restore InconsistentNaming
 
-        public static unsafe uint ComputeHash(uint seed, byte[] data)
+        public static unsafe uint ComputeHash(uint seed, int[] data)
         {
             Assert.IsNotNull(data);
 
-            fixed ( byte* pData = &data[0] )
+            fixed ( int* pData = &data[0] )
             {
                 return __inline__XXH32(seed, pData, data.Length);
             }
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static unsafe uint __inline__XXH32(uint seed, byte* input, int len)
+        private static unsafe uint __inline__XXH32(uint seed, int* input, int len)
         {
             uint h32;
 
-            if (len >= 16)
+            if (len >= 4)
             {
                 var end = input + len;
-                var limit = end - 15;
+                var limit = end - 3;
 
-                var v1 = seed + XxhPrime321 + XxhPrime322;
-                var v2 = seed + XxhPrime322;
+                var v1 = seed + XxhPrime32_1 + XxhPrime32_2;
+                var v2 = seed + XxhPrime32_2;
                 var v3 = seed + 0;
-                var v4 = seed - XxhPrime321;
+                var v4 = seed - XxhPrime32_1;
 
                 do
                 {
-                    v1 = XXH32_round(v1, *(uint*) (input + 0));
-                    v2 = XXH32_round(v2, *(uint*) (input + 4));
-                    v3 = XXH32_round(v3, *(uint*) (input + 8));
-                    v4 = XXH32_round(v4, *(uint*) (input + 16));
+                    v1 = XXH32_round(v1, *(uint*) input);
+                    ++input;
 
-                    input += 16;
+                    v2 = XXH32_round(v2, *(uint*) input);
+                    ++input;
+
+                    v3 = XXH32_round(v3, *(uint*) input);
+                    ++input;
+
+                    v4 = XXH32_round(v4, *(uint*) input);
+                    ++input;
                 } while (input < limit);
 
-                h32 = XXH_rotl32(v1, 1) + XXH_rotl32(v2, 7) + XXH_rotl32(v3, 12) + XXH_rotl32(v4, 18);
+                h32 = XXH_left_rotation(v1, 1) + XXH_left_rotation(v2, 7) + XXH_left_rotation(v3, 12) +
+                      XXH_left_rotation(v4, 18);
             }
             else
             {
-                h32 = seed + XxhPrime325;
+                h32 = seed + XxhPrime32_5;
             }
 
-            h32 += (uint) len;
+            h32 += (uint) len * 4;
 
             // XXH32_finalize
-            len &= 15;
-            while (len >= 4)
-            {
-                h32 += *(uint*) input * XxhPrime323;
-                input += 4;
-                h32 = XXH_rotl32(h32, 17) * XxhPrime324;
-                len -= 4;
-            }
-
+            len &= 3;
             while (len > 0)
             {
-                h32 += *input * XxhPrime325;
+                h32 += *(uint*) input * XxhPrime32_3;
+                h32 = XXH_left_rotation(h32, 17) * XxhPrime32_4;
+
                 ++input;
-                h32 = XXH_rotl32(h32, 11) * XxhPrime321;
                 --len;
             }
 
             // XXH32_avalanche
             h32 ^= h32 >> 15;
-            h32 *= XxhPrime322;
+            h32 *= XxhPrime32_2;
             h32 ^= h32 >> 13;
-            h32 *= XxhPrime323;
+            h32 *= XxhPrime32_3;
             h32 ^= h32 >> 16;
 
             return h32;
@@ -86,12 +87,12 @@ namespace Math
 
         private static uint XXH32_round(uint v2, uint input)
         {
-            v2 += input * XxhPrime322;
-            return XXH_rotl32(v2, 13) * XxhPrime321;
+            v2 += input * XxhPrime32_2;
+            return XXH_left_rotation(v2, 13) * XxhPrime32_1;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private static uint XXH_rotl32(uint x, int r)
+        private static uint XXH_left_rotation(uint x, int r)
         {
             return (x << r) | (x >> (32 - r));
         }
