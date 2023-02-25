3b89849ca6ff607c16b649eea66f287065e187f7
Almaz K <auqolaq@gmail.com>
2023-02-25 21:13:51 +0300

    Refactoring input system. And little bit of clean up.

--- Assets/Scripts/Game/Systems/PlayersInputQueueSystem.cs
+++ Assets/Scripts/Game/Systems/PlayersInputQueueSystem.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections.Generic;
 using App;
+using Game.Components;
+using Game.Components.Entities;
 using Infrastructure.Services.Input;
 using Input;
 using Leopotam.Ecs;
 using Level;
+using Math.FixedPointMath;
 using Unity.Mathematics;
+using UnityEngine.Assertions;
 
 namespace Game.Systems
 {
@@ -31,27 +35,30 @@ namespace Game.Systems
             playerInputProvider.OnBombBlastActionEvent += OnBombBlastAction;
         }
 
-        public void UnregisterPlayerInputProvider(IPlayerInputProvider playerInputProvider)
+        public void UnsubscribePlayerInputProvider(IPlayerInputProvider playerInputProvider)
         {
-            playerInputProvider.OnMoveActionEvent -= OnMoveAction;
-            playerInputProvider.OnBombPlantActionEvent -= OnBombPlantAction;
             playerInputProvider.OnBombBlastActionEvent -= OnBombBlastAction;
+            playerInputProvider.OnBombPlantActionEvent -= OnBombPlantAction;
+            playerInputProvider.OnMoveActionEvent -= OnMoveAction;
         }
 
         public void Run()
         {
             using var _ = Profiling.PlayersInputProcess.Auto();
 
-            foreach (var (provider, action) in _playersInputActions)
+            foreach (var (inputProvider, inputAction) in _playersInputActions)
             {
-                if (!_inputService.TryGetRegisteredPlayerTag(provider, out var playerTag))
+                if (!_inputService.TryGetRegisteredPlayerTag(inputProvider, out var playerTag))
+                {
+                    UnsubscribePlayerInputProvider(inputProvider);
                     continue;
+                }
 
                 if (!_world.Players.TryGetValue(playerTag, out var player))
                     continue;
 
                 // :TODO: check whether player is active
-                player.ApplyInputAction(_world, action);
+                ApplyInputAction(_world, player, in inputAction);
             }
 
             _playersInputActions.Clear();
@@ -70,6 +77,37 @@ namespace Game.Systems
         private void OnBombBlastAction(IPlayerInputProvider inputProvider)
         {
             _playersInputActions.Add((inputProvider, new PlayerInputAction { BombBlast = true }));
+        }
+
+        private static void ApplyInputAction(World world, IPlayer player, in PlayerInputAction inputAction)
+        {
+            ApplyMoveAction(world, player, inputAction.MovementVector);
+
+            if (inputAction.BombPlant)
+            {
+                ref var transformComponent = ref player.HeroEntity.Get<TransformComponent>();
+                var _ = world.OnPlayerBombPlant(player, transformComponent.WorldPosition); // :TODO: fix?
+            }
+
+            if (inputAction.BombBlast)
+                world.OnPlayerBombBlast(player);
+        }
+
+        private static void ApplyMoveAction(World world, IPlayer player, float2 value)
+        {
+            var heroEntity = player.HeroEntity;
+
+            ref var transformComponent = ref heroEntity.Get<TransformComponent>();
+            ref var movementComponent = ref heroEntity.Get<MovementComponent>();
+            ref var entityComponent = ref heroEntity.Get<EntityComponent>();
+
+            if (math.lengthsq(value) > 0)
+            {
+                transformComponent.Direction = (int2) math.round(value);
+                movementComponent.Speed = entityComponent.InitialSpeed * entityComponent.SpeedMultiplier;
+            }
+            else
+                movementComponent.Speed = fix.zero;
         }
     }
 }
